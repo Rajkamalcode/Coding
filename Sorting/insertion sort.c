3c3cb6014c6ccbef02f17404807433b3d0f563e3
Rajkamalcode <126259950+Rajkamalcode@users.noreply.github.com>
2023-08-27 21:33:11 +0530

    Update insertion sort.c

--- Sorting/insertion sort.c
+++ Sorting/insertion sort.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
-int arr[] = {15, 6, 7, 18, 29, 10, 11};
-
-int main(){
-    int i,j,key;
-    int n=sizeof(arr)/sizeof(arr[0]);
+void ins_sort(int n, int arr[]){
+    int i, j,key;
     for(i=0;i<n;i++){
-        key=arr[i];
         j=i-1;
+        key=arr[i];
 
-        while(j>=0 && arr[j]>key){
+        while(j>0 && arr[j]>key){
             arr[j+1]=arr[j];
             j--;
         }
         arr[j+1]=key;
     }
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
 
+}
+
+int main(){
+    int n;int i;
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter array of N numbers\n");
+    for(i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    int size=sizeof(arr)/sizeof(arr[0]);
+    ins_sort(size, arr);
+    printf("After Sorting\n");
+    for(i=0;i<n;i++){
+        printf("%d", arr[i]);
+    }
+    return 0;
 
 }
